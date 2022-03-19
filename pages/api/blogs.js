ee7cd2ee1924b4fc70a5e04b6befdf78c0435292
PranxXxD <71432749+PranxXxD@users.noreply.github.com>
2022-03-19 16:43:10 +0530

    Blogs Content
    Added the dynamic data fetching part in blog component and also removed the static blogs in the blogs.js also added the dynamic links to the each blog

--- pages/api/blogs.js
+++ pages/api/blogs.js
@@ -1,13 +1,19 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import * as fs from "fs";
 
-export default function handler(req, res) {
-  // console.log(req);
-  fs.readdir("blogdata", (err, data) => {
-    if (err) {
-      res.status(500).json({ error: "Internal Server Error" });
-    }
-    console.log(data);
-    res.status(200).json(data);
-  });
+export default async function handler(req, res) {
+  let data = await fs.promises.readdir("blogdata");
+  let myfile;
+  let allblogs = [];
+  for (let index = 0; index < data.length; index++) {
+    const item = data[index];
+    myfile = await fs.promises.readFile("blogdata/" + item, "utf-8");
+    allblogs.push(JSON.parse(myfile));
+  }
+  // fs.readdir("blogdata", (err, data) => {
+  //   if (err) {
+  //     res.status(500).json({ error: "Internal Server Error" });
+  //   }
+  //   console.log(data);
+  res.status(200).json(allblogs);
 }
--- pages/blog.js
+++ pages/blog.js
@@ -1,24 +1,42 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styles from "../styles/blog.module.css";
 import Link from "next/Link";
+import { ColoredLine } from "../components/hr";
 
 //step 1: collect all files from the blogdata directory
 //step 2 : Iterate through them and display them
 
-const profile = () => {
+const blog = () => {
+  const [blogs, setBlogs] = useState([]);
+  useEffect(() => {
+    // console.log("UseEffect is running");
+    fetch("http://localhost:3000/api/blogs")
+      .then((a) => {
+        return a.json();
+      })
+      .then((parsed) => {
+        setBlogs(parsed);
+      });
+  }, []);
+
   return (
     <div className={styles.container}>
       <main className={styles.main}>
         <div className={styles.blog}>
           <h2>Mostly Viewed Blogs</h2>
-          <div className={styles.blogItems}>
-            <Link href={"/blogpost/iPhoneSE-2022"}>
-              <h3>Apple new release</h3>
-            </Link>
-            <p>The new release of apple SE 2022 Edition</p>
-          </div>
+          <ColoredLine color="black" />
+          {blogs.map((blogitem) => {
+            return (
+              <div className={styles.blogItems} key={blogitem.slug}>
+                <Link href={`/blogpost/${blogitem.slug}`}>
+                  <h3>{blogitem.Name}</h3>
+                </Link>
+                <p>{blogitem.content.substr(0, 130)}.....</p>
+              </div>
+            );
+          })}
 
-          <div className={styles.blogItems}>
+          {/* <div className={styles.blogItems}>
             <h3>Apple new release</h3>
             <p>The new release of apple SE 2022 Edition</p>
           </div>
@@ -31,11 +49,11 @@ const profile = () => {
           <div className={styles.blogItems}>
             <h3>Apple new release</h3>
             <p>The new release of apple SE 2022 Edition</p>
-          </div>
+          </div> */}
         </div>
       </main>
     </div>
   );
 };
 
-export default profile;
+export default blog;
