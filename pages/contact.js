a428c57c396686a54b776bf84c27a0e1f89a1599
PranxXxD <71432749+PranxXxD@users.noreply.github.com>
2022-03-22 20:50:34 +0530

    Contact page
    Added the contact form in contact component also added the POST request functionality which uses fetch API to POST data in a Json format inside the contact data folder.

--- pages/contact.js
+++ pages/contact.js
@@ -1,7 +1,119 @@
-import React from "react";
+import React, { useState } from "react";
+import styles from "../styles/contact.module.css";
 
 const contact = () => {
-  return <div>contact</div>;
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+  const [desc, setDesc] = useState("");
+
+  const handleSubmit = (e) => {
+    // prevent refreshing the page after submiting
+    e.preventDefault();
+    // console.log(name, phone, email, desc);
+
+    // fetch the post req from the from
+    const data = { name, email, phone, desc };
+    fetch("http://localhost:3000/api/postcontact/", {
+      method: "POST", // or 'PUT'
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    })
+      .then((response) => response.text())
+      .then((data) => {
+        console.log("Success:", data);
+        // display alert window after submitting
+        alert("Thank you for contacting us");
+        // refers the input values of each field
+        setName("");
+        setEmail("");
+        setPhone("");
+        setDesc("");
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  };
+  // set the values of the input fields on a real time typing
+  const handleChange = (e) => {
+    if (e.target.name == "name") {
+      setName(e.target.value);
+    } else if (e.target.name == "email") {
+      setEmail(e.target.value);
+    } else if (e.target.name == "phone") {
+      setPhone(e.target.value);
+    } else if (e.target.name == "desc") {
+      setDesc(e.target.value);
+    }
+  };
+
+  return (
+    <div className={styles.container}>
+      <h1>Contact US</h1>
+      <form onSubmit={handleSubmit}>
+        <div className={styles.mb3}>
+          <label htmlFor="name" className={styles.formlabel}>
+            Name
+          </label>
+          <input
+            type="name"
+            className="form-control"
+            id="name"
+            name="name"
+            value={name}
+            onChange={handleChange}
+          />
+          <div id="name" className="form-text"></div>
+        </div>
+        <div className={styles.mb3}>
+          <label htmlFor="exampleInputEmail1" className={styles.formlabel}>
+            Email address
+          </label>
+          <input
+            type="email"
+            className="form-control"
+            id="email"
+            aria-describedby="emailHelp"
+            name="email"
+            value={email}
+            onChange={handleChange}
+          />
+          <div id="emailHelp" className="form-text">
+            We'll never share your email with anyone else.
+          </div>
+        </div>
+        <div className={styles.mb3}>
+          <label htmlFor="phone" className={styles.formlabel}>
+            Phone
+          </label>
+          <input
+            type="phone"
+            className="form-control"
+            id="phone"
+            name="phone"
+            value={phone}
+            onChange={handleChange}
+          />
+        </div>
+        <div className="form-floating">
+          <label htmlFor="desc">Comments</label>
+          <textarea
+            className="form-control"
+            placeholder="Leave a comment here"
+            id="desc"
+            name="desc"
+            value={desc}
+            onChange={handleChange}
+          />
+        </div>
+        <button type="submit" className="btn btn-primary">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default contact;
