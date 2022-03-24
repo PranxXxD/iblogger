6d6924b40bb36cc4be6c759c23d5d35909060c51
PranxXxD <71432749+PranxXxD@users.noreply.github.com>
2022-03-25 00:03:25 +0530

    Infinite Scroll
    Added infinite scroll in blog.js component which uses to fetch the data and display it while scrolling time also added the new json data files in blogdata folder

--- pages/blog.js
+++ pages/blog.js
@@ -3,6 +3,7 @@ import styles from "../styles/blog.module.css";
 import Link from "next/Link";
 import { ColoredLine } from "../components/hr";
 import * as fs from "fs";
+import InfiniteScroll from "react-infinite-scroll-component";
 
 //step 1: collect all files from the blogdata directory
 //step 2 : Iterate through them and display them
@@ -21,13 +22,34 @@ const blog = (props) => {
   //       setBlogs(parsed);
   //     });
   // }, []);
+  const [count, setCount] = useState(2);
 
+  const fetchData = async () => {
+    // a fake async api call like which sends
+    // 20 more records in 1.5 secs
+    let d = await fetch(`http://localhost:3000/api/blogs/?count=${count + 2}`);
+    setCount(count + 2);
+    let data = await d.json();
+    setBlogs(data);
+  };
   return (
     <div className={styles.container}>
       <main className={styles.main}>
         <div className={styles.blog}>
           <h2>Mostly Viewed Blogs</h2>
           <ColoredLine color="black" />
+        </div>
+        <InfiniteScroll
+          dataLength={blogs.length} //This is important field to render the next data
+          next={fetchData}
+          hasMore={props.allCount !== blogs.length}
+          loader={<h4>Loading...</h4>}
+          endMessage={
+            <p style={{ textAlign: "center" }}>
+              <b>Yay! You have seen it all</b>
+            </p>
+          }
+        >
           {blogs.map((blogitem) => {
             return (
               <div className={styles.blogItems} key={blogitem.slug}>
@@ -40,7 +62,7 @@ const blog = (props) => {
               </div>
             );
           })}
-        </div>
+        </InfiniteScroll>
       </main>
     </div>
   );
@@ -58,11 +80,12 @@ const blog = (props) => {
 // Method for the static side rendering
 export async function getStaticProps(context) {
   let data = await fs.promises.readdir("blogdata");
+  let allCount = data.length;
   let myfile;
   let allblogs = [];
 
   // fetch the data from the dir and storing in the item
-  for (let index = 0; index < data.length; index++) {
+  for (let index = 0; index < 2; index++) {
     const item = data[index];
 
     // read  data from the file
@@ -73,7 +96,7 @@ export async function getStaticProps(context) {
     allblogs.push(JSON.parse(myfile));
   }
   return {
-    props: { allblogs }, // will be passed to the page component as props
+    props: { allblogs, allCount }, // will be passed to the page component as props
   };
 }
 
