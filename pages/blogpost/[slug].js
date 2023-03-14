5fcc14e8c3fbce8a58a680fc44bea53b78695a82
Ramesh Alishetty <saipranayalishetty@gmail.com>
2023-03-14 22:22:21 +0530

    Update [slug].js

--- pages/blogpost/[slug].js
+++ pages/blogpost/[slug].js
@@ -1,18 +1,22 @@
 import React, { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 import { ColoredLine } from "../../components/hr";
-import * as fs from "fs";
+import Banner1 from "../../assets/Banner1.jpg";
 import Image from "next/image";
-import iphone12pro from "../../assets/iphone12pro.jpg";
+import Blogs from "../../models/Blogs";
+import mongoose from "mongoose";
 
 //step 1 : Find the file corresponding to the slug
 //step 2: populate them inside the page
 
-const Slug = (props) => {
+const Slug = ({allblogs}) => {
+  const router = useRouter();
   // console.log(router.query);
-  const [blog, setBlog] = useState(props.myBlog);
 
-  //display the markup code in text fromat
+  // const [blog, setBlog] = useState(props.myBlog);
+
+
+  // //display the markup code in text fromat
   function createMarkup(c) {
     return { __html: c };
   }
@@ -39,21 +43,19 @@ const Slug = (props) => {
           <div className="text-center lg:w-2/3 w-full">
             <Image
               className="lg:w-2/6 md:w-3/6 m-20 object-cover object-center rounded-2xl"
-              alt="iphone12pro"
-              src={iphone12pro}
+              alt=""
+              src={Banner1}
               width={900}
               height={400}
             />
             <h1 className="title-font sm:text-4xl text-3xl m-6 font-medium text-gray-900">
-              {blog && blog.Title}
+              {allblogs.title}
             </h1>
             <hr />
-            {blog && (
               <div
                 className="mb-8 py-4 leading-relaxed text-md text-gray-500 font-sans p-4"
-                dangerouslySetInnerHTML={createMarkup(blog.content)}
+                dangerouslySetInnerHTML={createMarkup(allblogs.desc)}
               ></div>
-            )}
           </div>
         </div>
       </section>
@@ -61,39 +63,56 @@ const Slug = (props) => {
   );
 };
 // Method for the Server side Rendereing
-// export async function getServerSideProps(context) {
-//   // console.log(context.query);
-//   const { slug } = context.query; //context.query is an object of the query string passed in the page's URL
-//   // let data = await fetch(`http://localhost:3000/api/getblogs?slug=${slug}`);
-//   // let myBlog = await data.json(); //covert the data into a json format
-//   return {
-//     props: { myBlog }, // will be passed to the page component as props
-//   };
-// }
+export async function getServerSideProps(context) {
+  // console.log(context.query);
+  let error = null
+  if (!mongoose.connections[0].readyState) {
+    await mongoose.connect(process.env.MONGO_URI);
+  }
+  let allblogs = await Blogs.findOne({ slug: context.query.slug });
+  // console.log(context.query.slug)
+   //display the error msg if user accessing the product which is not in database
+   if (allblogs == null) {
+    return {
+      props: {
+        error: 404,
+      }, // will be passed to the page component as props
+    };
+  }
+
+
+  ; //context.query is an object of the query string passed in the page's URL
+  
+  return {
+    props: {
+      allblogs: JSON.parse(JSON.stringify(allblogs)),
+    }, // will be passed to the page component as props
+  };
+}
 
 // Method for the Static side Rendering
 
-export async function getStaticPaths() {
-  // Dynamically fetcing data from the blogdata
-  let allblg = await fs.promises.readdir(`blogdata`);
-  allblg = allblg.map((item) => {
-    // console.log(item);
-    return { params: { slug: item.split(".")[0] } };
-  });
-  return {
-    paths: allblg,
-    fallback: true, // false or 'blocking'
-  };
-}
-// sends props to the server to fetch the whole data
-export async function getStaticProps(context) {
-  // console.log(context);
-  const { slug } = context.params;
+// export async function getStaticPaths() {
+//   // Dynamically fetcing data from the blogdata
+//   let allblg = await fs.promises.readdir(`blogdata`);
+//   allblg = allblg.map((item) => {
+//     // console.log(item);
+//     return { params: { slug: item.split(".")[0] } };
+//   });
+//   return {
+//     paths: allblg,
+//     fallback: true, // false or 'blocking'
+//   };
+// }
+// // sends props to the server to fetch the whole data
+// export async function getStaticProps(context) {
+//   // console.log(context);
+//   const { slug } = context.params;
 
-  let myBlog = await fs.promises.readFile(`blogdata/${slug}.json`, "utf-8");
-  return {
-    props: { myBlog: JSON.parse(myBlog) }, // will be passed to the page component as props
-  };
-}
+//   let myBlog = await fs.promises.readFile(`blogdata/${slug}.json`, "utf-8");
+//   return {
+//     props: { myBlog: JSON.parse(myBlog) }, // will be passed to the page component as props
+//   };
+// }
 
 export default Slug;
