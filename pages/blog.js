0a8b260956b4796bf91b083cd71fdbbd88738aa0
Ramesh Alishetty <saipranayalishetty@gmail.com>
2023-03-18 20:28:19 +0530

    Update blog.js

--- pages/blog.js
+++ pages/blog.js
@@ -7,7 +7,7 @@ import mongoose from "mongoose";
 //step 1: collect all files from the blogdata directory
 //step 2 : Iterate through them and display them
 
-const Blog = ({JSblogs}) => {
+const Blog = ({allblogs}) => {
   // console.log(JSblogs);
  
   return (
@@ -15,29 +15,27 @@ const Blog = ({JSblogs}) => {
       <section className="text-gray-600 body-font overflow-hidden min-h-screen ">
           <div className="flex flex-col container px-5 py-8 mx-auto">
             <div className="sm:flex sm:w-1/3 items-stretch justify-center m-auto sm:flex-col cursor-pointer">
-              {Object.keys(JSblogs).map((item) => {
+              {Object.keys(allblogs).map((item) => {
                 return (
-                  <Link key={item._id} href={`/blogs/${JSblogs[item].slug}`}>
+                  <Link key={item._id} href={`/blogpost/${allblogs[item].slug}`}>
                   <div
                     className="flex-grow sm:text-left text-center rounded-2xl px-4 mt-2 sm:mt-0 hover:bg-slate-200 shadow-md transition hover:ease-in"
-                    // key={JSblogs[item].slug}
+                    key={allblogs[item].slug}
                   >
-                    <Link href={`/blogs/${JSblogs[item].slug}`}>
                       <h3 className="md:text-2xl text-xl md:mt-6 py-4 font-medium text-gray-900 title-font">
-                        {JSblogs[item].title}
+                        {allblogs[item].title}
                       </h3>
-                    </Link>
                     <p className="leading-relaxed text-base w-auto">
-                      {JSblogs[item].desc.substr(0, 130)}
+                      {allblogs[item].desc.substr(0, 130)}
                     </p>
                     <a className="m-2 text-indigo-500 inline-flex items-center">
                       Learn More
                       <svg
                         fill="none"
                         stroke="currentColor"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        stroke-width="2"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
                         className="w-4 h-4 ml-2"
                         viewBox="0 0 24 24"
                       >
@@ -60,29 +58,29 @@ export async function getServerSideProps(context) {
   if (!mongoose.connections[0].readyState) {
     await mongoose.connect(process.env.MONGO_URI);
   }
-  let JSblogs = await Blogs.find();
+  let allblogs = await Blogs.find();
   // console.log(products);
   // jsBlg is an object
-  let jsBlg = {};
+  let blogs = {};
   // looping through the product array
-  for (let item of JSblogs) {
+  for (let item of allblogs) {
     // taking title as a key and jsBlg as an object
-    if (item.title in jsBlg) {
-      if (!jsBlg[item.title].category.includes(item.category)) {
-        jsBlg[item.title].category.push(item.category);
+    if (item.title in blogs) {
+      if (!blogs[item.title].category.includes(item.category)) {
+        blogs[item.title].category.push(item.category);
       }
     }
     // display the blog if the category is available
     else {
-       jsBlg[item.title] = JSON.parse(JSON.stringify(item));
-      if (item.title) {
-        jsBlg[item.title].category = [item.category];
-      }
+      blogs[item.title] = JSON.parse(JSON.stringify(item));
+      // if (item.title) {
+      //   blogs[item.title].category = [item.category];
+      // }
     }
   }
   return {
     props: {
-      JSblogs: JSON.parse(JSON.stringify(jsBlg)),
+      allblogs: JSON.parse(JSON.stringify(blogs)),
     }, // will be passed to the page component as props
   };
 }
