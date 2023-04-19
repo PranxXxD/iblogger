530fffbeb10a10e8edde24addece2c6e49362175
Ramesh Alishetty <saipranayalishetty@gmail.com>
2023-04-19 21:27:57 +0530

    added the side content and social content

--- pages/index.js
+++ pages/index.js
@@ -8,6 +8,8 @@ import { stringify } from "postcss";
 import mongoose from "mongoose";
 import Blogs from "../models/Blogs"
 import Link from "next/Link";
+import { SiBootstrap, SiCss3, SiFacebook, SiHtml5, SiInstagram, SiJavascript, SiJquery, SiLeetcode, SiLinkedin, SiNextdotjs, SiPinterest, SiReact, SiReddit, SiTailwindcss, SiTwitter } from "react-icons/si"
+
 
 const Home = ({ BlogRes }) => {
   // console.log(JsBlog,PyBlog)
@@ -21,7 +23,7 @@ const Home = ({ BlogRes }) => {
       </Head>
 
       <main className="m-auto">
-        <section className="text-gray-600 body-font m-auto">
+        <section className="text-gray-600 body-font">
           <div className="container px-5 py-6 mx-auto w-auto">
             <div className="text-center mb-20">
               <h1 className="sm:text-3xl text-2xl font-medium title-font text-gray-900 mb-4">
@@ -32,156 +34,150 @@ const Home = ({ BlogRes }) => {
                 <div className="w-16 h-1 rounded-full bg-violet-500 inline-flex"></div>
               </div>
             </div>
-            {/* Blog Content */}
-            <div id="blog" className="px-4 xl:px-0 py-0">
-              <div className="mx-auto container">
-                <div className="mt-12 lg:mt-24">
-                  <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8">
-                    {Object.keys(BlogRes).map((item, key) => {
-                      return (
-                        <div key={key}>
-                          <Image
-                            className="w-full rounded-t-2xl"
-                            src={Banner1}
-                            alt="JavaScript"
-                          />
-                          <div className="py-2 px-4 w-full flex justify-between bg-violet-700">
-                            <p className="text-sm text-white font-sans tracking-wide">
-                              {BlogRes[item].category}
-                            </p>
-                            <p className="text-sm text-white font-sans tracking-wide">
-                              {BlogRes[item].createdAt}
+            <div className="fixed top-[10rem] left-0 w-[3rem] min-h-full">
+              <div className=" flex flex-col shadow-xl justify-center items-center p-3 space-y-5 rounded-md">
+                <div className="flex flex-row hover:text-blue-500 text-sm font-light text-black  cursor-pointer "><SiFacebook className="text-xl" /></div>
+                <div className="flex flex-row hover:text-blue-400 text-sm font-light text-black cursor-pointer"><SiTwitter className="text-xl" /></div>
+                <div className="flex flex-row hover:text-blue-600 text-sm font-light text-black cursor-pointer"><SiLinkedin className="text-xl " /></div>
+                <div className="flex flex-row hover:text-red-700  text-sm font-light text-black cursor-pointer"><SiReddit className="text-xl" /></div>
+                <div className="flex flex-row hover:text-red-500  text-sm font-light text-black cursor-pointer"> <SiPinterest className="text-xl" /></div>
+                <div className="flex flex-row hover:text-orange-500 text-sm font-light text-black cursor-pointer"><SiLeetcode className="text-xl" /></div>
+                <div className="flex flex-row hover:text-pink-500 text-sm font-light text-black cursor-pointer"><SiInstagram className="text-xl" /></div>
 
-                            </p>
-                          </div>
-                          <div className="bg-white px-10 py-6 rounded-bl-xl rounded-br-xl">
-                            <h1 className="text-xl text-gray-900 font-semibold tracking-wider">
-                              {BlogRes[item].title}
-                            </h1>
-                            <p className="text-gray-700 text-sm lg:text-sm  lg:leading-8 tracking-wide mt-6 w-11/12">
-                              {BlogRes[item].desc.substr(0, 50)}
-                            </p>
-                            <div className="w-full mt-4 justify-end flex items-center cursor-pointer">
-                              <Link href={"/blog"}><p className="text-base tracking-wide text-indigo-500">
-                                Read more
-                              </p></Link>
-                              <svg
-                                className="ml-3 lg:ml-6"
-                                xmlns="http://www.w3.org/2000/svg"
-                                width={20}
-                                height={18}
-                                viewBox="0 0 20 18"
-                                fill="none"
-                              >
-                                <path
-                                  d="M11.7998 1L18.9998 8.53662L11.7998 16.0732"
-                                  stroke="#4338ca"
-                                  strokeWidth={2}
-                                  strokeLinecap="round"
-                                  strokeLinejoin="round"
-                                />
-                                <path
-                                  d="M1 8.53662H19"
-                                  stroke="#4338ca"
-                                  strokeWidth={2}
-                                  strokeLinecap="round"
-                                  strokeLinejoin="round"
-                                />
-                              </svg>
-                            </div>
-                            <div className="h-5 w-2" />
-                          </div>
-                        </div>
-                      )
-                    })}
-                    <div>
-                  </div>
-                    {/* card 1st row */}
-                    <div className="mx-auto container">
-                      <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2 gap-8">
-                        {Object.keys(BlogRes).map((item, key) => {
-                          return (<div className="w-auto" key={key}>
-                            <Image
-                              className="w-full rounded-t-2xl"
-                              src={webdevelopment}
-                              alt="games"
-                            />
-                            <div className="py-2 px-4 w-full flex justify-between bg-indigo-700">
-                              <p className="text-sm text-white font-sans tracking-wide">
-                                {BlogRes[item].title}
-                              </p>
-                              <p className="text-sm text-white font-sans tracking-wide">
-                                {BlogRes[item].createdAt}
-                              </p>
-                            </div>
-                            <div className="bg-white px-3  lg:px-6 py-4 rounded-bl-xl rounded-br-xl">
-                              <h1 className="text-lg text-gray-900 font-sans tracking-wider">
-                                {BlogRes[item].title}
-                              </h1>
-                              <p className="text-gray-700 text-sm  lg:text-sm  lg:leading-8 pr-4 tracking-wide mt-2">
-                                {BlogRes[item].desc.substr(0, 50)}
-                              </p>
-                            </div>
-                          </div>
-                          )
-                        })}
-                      </div>
+              </div>
+            </div>
+            {/* Blog Content */}
+            <div id="blog" className="flex ml-12 px-6 xl:px-0 py-0 flex-row w-1/2">
+              <div className="mx-auto container">
+                <div className="lg:fixed lg:top-1/3 lg:right-[9rem] w-[9rem] lg:min-h-full">
+                  <div className="flex flex-col space-y-2">
+                    <span className="text-xs font-semibold mb-2">CATEGORIES</span>
+                    <div className="w-[22rem] grid grid-cols-3 md:w-[28rem] md:space-y-2 md:grid-cols-4 lg:flex lg:flex-col space-y-2">
+                      <div className="flex flex-row hover:text-orange-500 text-sm font-light text-black mt-2  cursor-pointer"><SiHtml5 className="text-xl mr-4 " />HTML</div>
+                      <div className="flex flex-row hover:text-blue-500 text-sm font-light text-black cursor-pointer"><SiCss3 className="text-xl mr-4" />CSS</div>
+                      <div className="flex flex-row hover:text-violet-500 text-sm font-light text-black cursor-pointer"><SiBootstrap className="text-xl  mr-4" />BootStrap</div>
+                      <div className="flex flex-row hover:text-blue-300 text-sm font-light text-black cursor-pointer"><SiTailwindcss className="text-xl mr-4" />Tailwind</div>
+                      <div className="flex flex-row hover:text-yellow-500  text-sm font-light text-black cursor-pointer"><SiJavascript className="text-xl mr-4" />JavaScript</div>
+                      <div className="flex flex-row hover:text-orange-300  text-sm font-light text-black cursor-pointer"> <SiJquery className="text-xl mr-4" />Jquery</div>
+                      <div className="flex flex-row hover:text-blue-400 text-sm font-light text-black cursor-pointer"><SiReact className="text-xl mr-4" />React.js</div>
+                      <div className="flex flex-row hover:text-black text-sm font-light text-gray-600 cursor-pointer"><SiNextdotjs className="text-xl mr-4" />Next.js</div>
                     </div>
                   </div>
+              </div>
+              <div className="mt-10 lg:ml-[10rem] ">
+                <div className="w-[20rem] grid md:w-[40rem] md:max-w-xl md:grid-cols-2 lg:grid-cols-1 gap-6">
+                  {Object.keys(BlogRes).map((item, key) => {
+                    return (
+                      <div key={key}>
+                        <Image
+                          className="w-full rounded-t-2xl"
+                          src={Banner1}
+                          alt="JavaScript"
+                        />
+                        <div className="py-2 px-4 w-full flex justify-between bg-violet-700">
+                          <p className="text-sm text-white font-sans tracking-wide">
+                            {BlogRes[item].category}
+                          </p>
+                          <p className="text-sm text-white font-sans tracking-wide">
+                            {BlogRes[item].createdAt}
+
+                          </p>
+                        </div>
+                        <div className="bg-white px-10 py-6 rounded-bl-xl rounded-br-xl">
+                          <h1 className="text-xl text-gray-900 font-semibold tracking-wider">
+                            {BlogRes[item].title}
+                          </h1>
+                          <p className="text-gray-700 text-sm lg:text-sm  lg:leading-8 tracking-wide mt-6 w-11/12">
+                            {BlogRes[item].desc.substr(0, 50)}
+                          </p>
+                          <div className="w-full mt-4 justify-end flex items-center cursor-pointer">
+                            <Link href={`/blogpost/${BlogRes[item].slug}`}><p className="text-base tracking-wide text-indigo-500">
+                              Read more
+                            </p></Link>
+                            <svg
+                              className="ml-3 lg:ml-6"
+                              xmlns="http://www.w3.org/2000/svg"
+                              width={20}
+                              height={18}
+                              viewBox="0 0 20 18"
+                              fill="none"
+                            >
+                              <path
+                                d="M11.7998 1L18.9998 8.53662L11.7998 16.0732"
+                                stroke="#4338ca"
+                                strokeWidth={2}
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                              />
+                              <path
+                                d="M1 8.53662H19"
+                                stroke="#4338ca"
+                                strokeWidth={2}
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                              />
+                            </svg>
+                          </div>
+                          <div className="h-5 w-2" />
+                        </div>
+                      </div>
+                    )
+                  })}
                 </div>
               </div>
             </div>
-
-            <button className="flex mx-auto mt-16 text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">
-              View More
-            </button>
           </div>
-        </section>
 
-        <section className="text-gray-600 body-font overflow-hidden ">
-          <div className="flex container px-5 py-24 mx-auto w-1/2">
-            <div className="-my-8 divide-y-2 divide-gray-100">
-              <div className="py-8 flex flex-wrap md:flex-nowrap">
-                <div className="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
-                  <span className="font-semibold title-font text-gray-700">
-                    CATEGORY
-                  </span>
-                  <span className="mt-1 text-gray-500 text-sm">
-                    12 Jun 2019
-                  </span>
-                </div>
-                <div className="md:flex-grow">
-                  <h2 className="text-2xl font-medium text-gray-900 title-font mb-2">
-                    Bitters hashtag waistcoat fashion axe chia unicorn
-                  </h2>
-                  <p className="leading-relaxed">
-                    Glossier echo park pug, church-key sartorial biodiesel
-                    vexillologist pop-up snackwave ramps cornhole. Marfa 3 wolf
-                    moon party messenger bag selfies, poke vaporware kombucha
-                    lumbersexual pork belly polaroid hoodie portland craft beer.
-                  </p>
-                  <a className="text-indigo-500 inline-flex items-center mt-4">
-                    Learn More
-                    <svg
-                      className="w-4 h-4 ml-2"
-                      viewBox="0 0 24 24"
-                      stroke="currentColor"
-                      strokeWidth="2"
-                      fill="none"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                    >
-                      <path d="M5 12h14"></path>
-                      <path d="M12 5l7 7-7 7"></path>
-                    </svg>
-                  </a>
-                </div>
+          <button className="flex mx-auto mt-16 text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">
+            View More
+          </button>
+        </div>
+      </section>
+
+      <section className="text-gray-600 body-font overflow-hidden ">
+        <div className="flex container px-5 py-24 mx-auto w-1/2">
+          <div className="-my-8 divide-y-2 divide-gray-100">
+            <div className="py-8 flex flex-wrap md:flex-nowrap">
+              <div className="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
+                <span className="font-semibold title-font text-gray-700">
+                  CATEGORY
+                </span>
+                <span className="mt-1 text-gray-500 text-sm">
+                  12 Jun 2019
+                </span>
+              </div>
+              <div className="md:flex-grow">
+                <h2 className="text-2xl font-medium text-gray-900 title-font mb-2">
+                  Bitters hashtag waistcoat fashion axe chia unicorn
+                </h2>
+                <p className="leading-relaxed">
+                  Glossier echo park pug, church-key sartorial biodiesel
+                  vexillologist pop-up snackwave ramps cornhole. Marfa 3 wolf
+                  moon party messenger bag selfies, poke vaporware kombucha
+                  lumbersexual pork belly polaroid hoodie portland craft beer.
+                </p>
+                <a className="text-indigo-500 inline-flex items-center mt-4">
+                  Learn More
+                  <svg
+                    className="w-4 h-4 ml-2"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                    fill="none"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  >
+                    <path d="M5 12h14"></path>
+                    <path d="M12 5l7 7-7 7"></path>
+                  </svg>
+                </a>
               </div>
             </div>
           </div>
-        </section>
-      </main>
-    </div>
+        </div>
+      </section>
+    </main>
+    </div >
   );
 }
 
